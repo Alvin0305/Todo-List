434051170639fc4d0477294c146411e4d392ba54
Alvin0305 <alvinanildas@gmail.com>
2025-10-18 12:01:17 +0530

    updated task model for hive

--- lib/models/task.dart
+++ lib/models/task.dart
@@ -1,5 +1,12 @@
+import 'package:hive/hive.dart';
+
+part 'task.g.dart';
+
+@HiveType(typeId: 0)
 class Task {
+  @HiveField(0)
   String name;
+  @HiveField(1)
   bool completed;
 
   Task({required this.name, required this.completed});
